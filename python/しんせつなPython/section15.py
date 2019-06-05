42a6fb06e870067afb55a7e00edf9894ee56fe4f
shuim <shunnsuke.imai@gmail.com>
2019-06-05 23:33:35 +0900

    しりとりプログラム

---
+++ python/しんせつなPython/section15.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+my_word = "しりとり"
+oshiri = "しりとり"[-1]
+print(my_word,"の『",oshiri,"』から始まる言葉を入力してください")
+
+my_word = input()
+print(my_word,"ですね")
+
+atama = my_word[0]
+print("最初の文字は",atama,"です")
